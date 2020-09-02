1cffa19aa9cf2fad24a42978fbe7bb997506e44e
Rowan Rathod <rowanrathod@nightwind-developments.co.nz>
2020-09-02 19:45:04 +1200

    Added more content to the second example
    It is not finished yet. I am adding it to finish off later.

---
+++ GladeProgramP2/gladeprogram.h
@@ -0,0 +1,16 @@
+//
+// Created by Rowan Rathod on 1/09/20.
+//
+
+#ifndef GLADEPROGRAMP2_GLADEPROGRAM_H
+#define GLADEPROGRAMP2_GLADEPROGRAM_H
+
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+
+gchar graphics_file_loc = "GPP2_Demo_Template.glade";
+
+#endif //GLADEPROGRAMP2_GLADEPROGRAM_H
--- GladeProgramP2/main.cpp
+++ GladeProgramP2/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include "gladeprogram.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+GtkWidget *window;
+GtkWidget *record_btn;
+GtkWidget *recording_label;
+GtkWidget *builder;
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new_from_file("GPP2_Demo_Template.glade");
+
     return 0;
 }
